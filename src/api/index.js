ad19102874c05aa00667a372fa1595576bb076b8
Thai Le <thailekha@yahoo.com>
2018-04-20 18:12:15 +0100

    ignore gantry port 3001

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@ import errorHandler from './errorHandler';
 const keyv = new Keyv();
 
 export default function buildAPI(server) {
-  const IGNORED_PORTS = ['22','111', `${server.server.address().port}`];
+  const IGNORED_PORTS = ['22','111', '3001', `${server.server.address().port}`];
   const IGNORED_PROGRAMS = ['rpc.statd'];
 
   server.use('/checkhost', checkhostHandler());
